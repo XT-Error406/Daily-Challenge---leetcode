4e1b1b0565f3b196fd239e82d39310f652ef4e59
Error404 <81263628+XT-Error406@users.noreply.github.com>
2022-08-19 04:08:31 +0530

    Create 1338. Reduce Array Size to The Half.cpp

---
+++ 1338. Reduce Array Size to The Half.cpp
@@ -0,0 +1,53 @@
+
+
+class Solution {
+public:
+   int miniset(vector<int>& arr){
+      int n = arr.size();
+      unordered_map<int,int>freq;
+      
+        for(int i=0;i<n;i++){
+            freq[arr[i]]++;
+           }
+       
+       multiset<int,greater<int>>m;
+       
+       for(auto i: freq){
+           
+           m.insert(i.second);
+       }
+       int count=0; int ans=0;
+       for(auto it=m.begin();ans*2<n;it++){
+           
+           ans+= *it;
+           count++;
+       }
+        return count;
+   }
+    
+    int minSetSize(vector<int>& arr) {
+
+//     int n = arr.size();
+        
+//       vector<int>freq(100005);     
+//       // given 10^5 o(1)  S.P
+        
+//         for(int i=0;i<n;i++){
+//             freq[arr[i]]++;
+//         }
+        
+//      sort(freq.rbegin(),freq.rend()); 
+        
+//        int ans=0;
+        
+//        for(int i=0;i<n;i++){
+           
+//             ans+=freq[i];
+           
+//             if(ans>=n/2) return(i+1);
+//         }
+//        return n;
+        
+      return miniset(arr);
+    }
+};
